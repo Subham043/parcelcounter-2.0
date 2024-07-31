6275825cb9bef2ac6118183ba820254a60781af4
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-07-31 16:23:54 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'parcelcounter2.0',
+  webDir: 'dist'
+};
+
+export default config;
