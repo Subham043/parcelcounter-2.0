1aafc592fdb53d98c36a07b8866f72e9c16598f6
Subham043 <sahasubham043@gmail.com>
2024-09-30 00:38:05 +0530

    pre build android

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package io.parcelcounter.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,25 +1,25 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.starter",
+  appId: "io.parcelcounter.app",
   appName: "Parcelcounter",
   webDir: "dist",
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      launchAutoHide: true,
-      launchFadeOutDuration: 1000,
-      backgroundColor: "#ffffff",
-      // androidScaleType: "CENTER",
-      showSpinner: true,
-      splashFullScreen: true,
-      splashImmersive: true,
-      androidSpinnerStyle: 'small',
-      spinnerColor: '#999999'
-      // layoutName: "launch_screen",
-      // useDialog: true,
-    },
-  },
+  // plugins: {
+  //   SplashScreen: {
+  //     launchShowDuration: 2000,
+  //     launchAutoHide: true,
+  //     launchFadeOutDuration: 1000,
+  //     backgroundColor: "#ffffff",
+  //     // androidScaleType: "CENTER",
+  //     showSpinner: true,
+  //     splashFullScreen: true,
+  //     splashImmersive: true,
+  //     androidSpinnerStyle: 'small',
+  //     spinnerColor: '#999999'
+  //     // layoutName: "launch_screen",
+  //     // useDialog: true,
+  //   },
+  // },
 };
 
 export default config;
